cce359493c2faa2f6fb2a2d612ee6f0a2272aecc
Ander Punnar <ander@kvlt.ee>
2022-08-08 11:00:06 +0300

    small fixes, change how libssl1.1 and libsdbus-c++0 are downloaded

--- mkosi/mkosi.skeleton/etc/profile.d/environment.sh
+++ mkosi/mkosi.skeleton/etc/profile.d/environment.sh
@@ -1,3 +1,4 @@
 export DISPLAY=:0
+export PATH="/opt/microsoft/intune/bin:$PATH"
 export PULSE_SERVER=unix:/run/host_pulse_native
-systemctl --user import-environment DISPLAY PULSE_SERVER
+systemctl --user import-environment DISPLAY PATH PULSE_SERVER
