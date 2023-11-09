2ab285e616d5f0ec57d3f05eb21889e52b9f7bbd
Kai Lueke <kailuke@microsoft.com>
2023-11-09 14:57:25 +0100

    Use Edge Stable

--- mkosi.skeleton/etc/profile.d/mkosi.sh
+++ mkosi.skeleton/etc/profile.d/mkosi.sh
@@ -4,7 +4,7 @@ export PULSE_SERVER=unix:/run/host_pulse_native
 export WAYLAND_DISPLAY=/run/host_wayland_0
 export PIPEWIRE_REMOTE=/run/host_pipewire_0
 
-alias edge='microsoft-edge-dev --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-features=WebRTCPipeWireCapturer'
+alias edge='microsoft-edge-stable --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-features=WebRTCPipeWireCapturer'
 systemctl --user import-environment DISPLAY PATH PULSE_SERVER WAYLAND_DISPLAY PIPEWIRE_REMOTE
 
 if ! systemctl -q --user is-active intune-agent.timer
