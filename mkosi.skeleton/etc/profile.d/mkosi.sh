59d49ff0d152c332213a2c1e061c9724c38423a4
Ander Punnar <ander@kvlt.ee>
2024-04-16 22:42:20 +0300

    add azvpn to path

--- mkosi.skeleton/etc/profile.d/mkosi.sh
+++ mkosi.skeleton/etc/profile.d/mkosi.sh
@@ -1,5 +1,5 @@
 export DISPLAY=:0
-export PATH="/opt/microsoft/intune/bin:$PATH"
+export PATH="/opt/microsoft/intune/bin:/opt/microsoft/microsoft-azurevpnclient:$PATH"
 export PULSE_SERVER=unix:/run/host_pulse_native
 
 systemctl --user import-environment DISPLAY PATH PULSE_SERVER
